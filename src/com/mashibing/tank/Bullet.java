b35a7f206fb7c47781b258c60def95e0b0b48bcb
Carho0523 <93450310+Carho0523@users.noreply.github.com>
2022-06-02 22:56:31 +0100

    定义子弹类，设置属性和方法。new出一颗新的子弹，在屏幕中画出来

--- src/com/mashibing/tank/Bullet.java
+++ src/com/mashibing/tank/Bullet.java
@@ -2,43 +2,26 @@ package com.mashibing.tank;
 
 import java.awt.*;
 
-public class Tank {
+public class Bullet {
+    private static final int SPEED = 10;
+    private static final int HEIGHT = 5,WIDTH = 5;
     private int x,y;
-    private Dir dir = Dir.DOWN;
-    private static final int SPEED = 5;
-    private boolean moving = false;
+    private Dir dir;
 
-    public Tank(int x,int y, Dir dir){
-        super();
+    public Bullet(int x,int y,Dir dir){
         this.x = x;
         this.y = y;
         this.dir = dir;
     }
 
-    public boolean isMoving() {
-        return moving;
-    }
-
-    public void setMoving(boolean moving) {
-        this.moving = moving;
-    }
-
-    public Dir getDir(){
-        return dir;
-    }
-
-    public void setDir(Dir dir){
-        this.dir = dir;
-    }
-
-
     public void paint(Graphics g) {
-        g.fillRect(x,y,50,50);
+        Color c = g.getColor();
+        g.setColor(Color.RED);
+        g.fillOval(x,y,WIDTH,HEIGHT);
         move();
     }
 
     public void move(){
-        if(!moving) return;
         switch(dir){
             case LEFT:
                 x -= SPEED;
@@ -54,6 +37,5 @@ public class Tank {
                 break;
         }
     }
-
 
 }
--- src/com/mashibing/tank/TankFrame.java
+++ src/com/mashibing/tank/TankFrame.java
@@ -9,6 +9,7 @@ import java.awt.event.WindowEvent;
 public class TankFrame extends Frame {
 
     Tank myTank = new Tank(200,200,Dir.DOWN);
+    Bullet b = new Bullet(300,300,Dir.DOWN);
 
     public TankFrame(){
         setVisible(true);
@@ -29,6 +30,7 @@ public class TankFrame extends Frame {
     @Override
     public void paint(Graphics g){
         myTank.paint(g);
+        b.paint(g);
     }
 
     class MykeyListener extends KeyAdapter{
