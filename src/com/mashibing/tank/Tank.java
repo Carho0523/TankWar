423ed4f608373b114786421849fca926f781707f
Carho0523 <93450310+Carho0523@users.noreply.github.com>
2022-06-02 22:46:26 +0100

    设置坦克是否移动的属性moving，如果四个方向都没有位移，坦克就属于静止状态

--- src/com/mashibing/tank/Tank.java
+++ src/com/mashibing/tank/Tank.java
@@ -6,12 +6,21 @@ public class Tank {
     private int x,y;
     private Dir dir = Dir.DOWN;
     private static final int SPEED = 10;
+    private boolean moving = false;
 
     public Tank(int x,int y, Dir dir){
         super();
         this.x = x;
         this.y = y;
         this.dir = dir;
+    }
+
+    public boolean isMoving() {
+        return moving;
+    }
+
+    public void setMoving(boolean moving) {
+        this.moving = moving;
     }
 
     public Dir getDir(){
@@ -25,6 +34,12 @@ public class Tank {
 
     public void paint(Graphics g) {
         g.fillRect(x,y,50,50);
+        move();
+
+    }
+
+    public void move(){
+        if(!moving) return;
         switch(dir){
             case LEFT:
                 x -= SPEED;
@@ -40,4 +55,6 @@ public class Tank {
                 break;
         }
     }
+
+
 }
